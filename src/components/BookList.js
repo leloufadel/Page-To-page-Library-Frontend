387f73ecca8f0fdcd20c5f8ca68fd3f210082215
Arch-Noize <santiagonoize@gmail.com>
2024-01-19 10:23:22 -0400

    Update BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/bookSlice';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
   if (books.books.length === 0) {
     return (
@@ -17,6 +19,9 @@ const BookList = () => {
           <li key={book.id}>
             <h3>{book.name}</h3>
             <p>{book.author}</p>
+            <button type="button" onClick={() => dispatch(removeBook(book.id))}>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
