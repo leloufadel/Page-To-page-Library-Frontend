41b321dc796c48735946ffa14b9f3e19da8fb10a
Arch-Noize <santiagonoize@gmail.com>
2024-01-19 08:38:32 -0400

    Add basic list

---
+++ src/components/BookList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const BookList = () => {
+  const books = useSelector((state) => state.books);
+
+  if (books.books.length === 0) {
+    return (
+      <h2>Currently, theres no Books!</h2>
+    );
+  }
+
+  return (
+    <div>
+      <ul>
+        {books.books.map((book) => (
+          <li key={book.item_id}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
