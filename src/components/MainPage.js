a6ff66f040ecb4f9defcd11c39348186c650357d
leloufadel <leloufadel@gmail.com>
2024-01-22 16:16:42 +0000

    style the scrolling

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -9,11 +9,9 @@ import 'swiper/css';
 import 'swiper/css/bundle';
 import '../stylesheets/mainpage.css';
 
-
-
 const MainPage = () => {
   const books = useSelector((state) => state.books);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage] = useState(1);
   const booksPerPage = 3;
 
   const startIndex = (currentPage - 1) * booksPerPage;
@@ -40,14 +38,17 @@ const MainPage = () => {
 
   return (
     <>
-     <div className="Books-package d-flex justify-content-center align-items-center">
-      <div className="text-center">
-        <h1 className="books-heading">Book library</h1>
-        <p className="books-sub-heading">Choose a book to see more details</p>
-      </div>
+      <div className="Books-package d-flex justify-content-center align-items-center">
+        <div className="text-center">
+          <h1 className="books-heading">Book library</h1>
+          <p className="books-sub-heading">Choose a book to see more details</p>
+        </div>
         <div className="Books-list">
           <button
-            type="button" className="swipe-btn-1" aria-label="Previous" onClick={handlePrevSlide}
+            type="button"
+            className="swipe-btn-1"
+            aria-label="Previous"
+            onClick={handlePrevSlide}
           >
             <img
               className="slide-button"
@@ -55,15 +56,12 @@ const MainPage = () => {
               alt="slide"
             />
           </button>
-          
+
           <Swiper
-            onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => {
-              console.log(swiper);
               swiperRef.current = swiper;
             }}
-            slidesPerView={2} 
-            // spaceBetween={10} 
+            slidesPerView={2}
           >
             {displayedBooks.map((book) => (
               <SwiperSlide key={book.id}>
@@ -83,17 +81,18 @@ const MainPage = () => {
             ))}
           </Swiper>
           <button
-            type="button" className="swipe-btn-2" aria-label="Next" onClick={handleNextSlide}
-// disabled={endIndex >= books.books.length}
-          
-        >
+            type="button"
+            className="swipe-btn-2"
+            aria-label="Next"
+            onClick={handleNextSlide}
+          >
             <img
-            className="slide-button"
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAO9JREFUSEu1ltENwjAQQ+1NYBOYBDoJZRLYhI7STYwsJShSmyZNyX2icu9sX08lOhc798cqQNILwAXAleR8ZIgcQKGpm79JPlshJUDsa1CTmhLgDOAD4ASgSc0mgCQlufkNwBjkTACG2myKgOiRpDuAx1411QCDVtSMpQXYBUjUWEm0zNnYMlu3qCZAoia+L/5pVU0zIGPZTNKb96tDgABJ7ZpIXv8CCIH3sUhSn5BXpvadGrbuVHUGknxdfTZcm6u5K4MwtS3xm+xaBNmswBc0mTq760cA3c9109Q1GcT9zt6YLVuKgNo/1zzX/aviCyWOnxkFdwrCAAAAAElFTkSuQmCC"
-            alt="slide"
-          />
+              className="slide-button"
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAO9JREFUSEu1ltENwjAQQ+1NYBOYBDoJZRLYhI7STYwsJShSmyZNyX2icu9sX08lOhc798cqQNILwAXAleR8ZIgcQKGpm79JPlshJUDsa1CTmhLgDOAD4ASgSc0mgCQlufkNwBjkTACG2myKgOiRpDuAx1411QCDVtSMpQXYBUjUWEm0zNnYMlu3qCZAoia+L/5pVU0zIGPZTNKb96tDgABJ7ZpIXv8CCIH3sUhSn5BXpvadGrbuVHUGknxdfTZcm6u5K4MwtS3xm+xaBNmswBc0mTq760cA3c9109Q1GcT9zt6YLVuKgNo/1zzX/aviCyWOnxkFdwrCAAAAAElFTkSuQmCC"
+              alt="slide"
+            />
           </button>
-          
+
         </div>
       </div>
     </>
