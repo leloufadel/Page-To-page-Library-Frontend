aa65f70e5bcdbb22e18a2dbe1ff51a7d39b5e4d8
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-22 23:02:52 +0300

    Delete commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
 import BookList from './components/BookList';
-// import { getBooks } from './redux/bookSlice';
-// import SideNav from './components/Navbar/navbar';
 import Splach from './components/Splach';
 import Login from './components/user/login';
 import Signup from './components/user/sighnup';
@@ -12,15 +9,8 @@ import Logout from './components/user/logout';
 import Home from './components/Home/home';
 
 function App() {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(getBooks());
-  // });
-
   return (
     <>
-      {/* <SideNav /> */}
       <Routes>
         <Route path="/" element={<Splach />} />
         <Route path="/login" element={<Login />} />
