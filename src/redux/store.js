bc206489da5b55970f453367621e84d7e5d31021
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-22 20:36:17 +0300

    Add userReducer  and modalReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { thunk } from 'redux-thunk';
 import booksReducer from './bookSlice';
+import userReducer from './user/userSlice';
+import modalReducer from './modalSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    users: userReducer,
+    modal: modalReducer,
   },
+
+  middleware: (/* getDefaultMiddleware */) => [thunk],
 });
 
 export default store;
