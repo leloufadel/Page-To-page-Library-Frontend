dcf7221357259b57b3e95277a9febb464033e7f6
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-22 23:09:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import BookList from './components/BookList';
+import { getBooks } from './redux/bookSlice';
+import SideNav from './components/Navbar/navbar';
 import Splach from './components/Splach';
 import Login from './components/user/login';
 import Signup from './components/user/sighnup';
@@ -9,8 +12,15 @@ import Logout from './components/user/logout';
 import Home from './components/Home/home';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  });
+
   return (
     <>
+      <SideNav />
       <Routes>
         <Route path="/" element={<Splach />} />
         <Route path="/login" element={<Login />} />
