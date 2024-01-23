a50c4c4e30996dbf78fe07b21b54fa79a4af64b8
leloufadel <leloufadel@gmail.com>
2024-01-23 20:50:59 +0000

    Create Form to add books

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -35,5 +35,42 @@ const BookForm = () => {
     setInfo('');
   };
 
+  return (
+    <div className="PackageForm">
+      <form className="input" onSubmit={handleSubmit}>
+      <div className="mb-3">
+<input type="text" placeholder="Name" id="nameInput" className="form-control" value={name} onChange={(e) => setName(e.target.value)} required />
+</div>
+<div className="mb-3">
+<input type="text" placeholder="Image URL" className="form-control" value={cover_photo} onChange={(e) => setImage(e.target.value)} required />
+
+</div>
+<div className="mb-3">
+<input type="text" placeholder="Author" id="nameInput" className="form-control" value={author} onChange={(e) => setAuthor(e.target.value)} required />
+</div>
+
+<div className="mb-3">
+<input type="text" placeholder="Pblisher" id="nameInput" className="form-control" value={publisher} onChange={(e) => setPublisher(e.target.value)} required />
+</div>
+<div className="mb-3">
+<input type="text" placeholder="publisherDate" id="nameInput" className="form-control" value={publish_date} onChange={(e) => setDate(e.target.value)} required />
+</div>
+<div className="mb-3">
+<input type="text" placeholder="Name" id="nameInput" className="form-control" value={category} onChange={(e) => setCategory(e.target.value)} required />
+
+</div>
+<div className="mb-3">
+<input type="text" placeholder="Name" id="nameInput" className="form-control" value={info} onChange={(e) => setInfo(e.target.value)} required />
+
+</div>
+
+        <button type="submit" className="form-btn">
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
+ 
