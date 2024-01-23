5d4377674abe1b6cffbc1abe363971caddc01517
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-23 20:11:50 +0200

    Fix linter errors

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FaFacebookF, FaVimeoV } from 'react-icons/fa';
-import { TiMediaPlayOutline } from 'react-icons/ti';
+// import { TiMediaPlayOutline } from 'react-icons/ti';
 import { BsTwitter } from 'react-icons/bs';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -13,7 +13,7 @@ const MainPage = () => {
   const books = useSelector((state) => state.books);
   const [currentPage] = useState(1);
   const booksPerPage = 3;
-  const booksPerPage = 4;
+  // const booksPerPage = 4;
 
   const startIndex = (currentPage - 1) * booksPerPage;
   const endIndex = startIndex + booksPerPage;
@@ -44,7 +44,6 @@ const MainPage = () => {
           <h1 className="books-heading">Book library</h1>
           <p className="books-sub-heading">Choose a book to see more details</p>
           <p className="books-line">------------------------</p>
-
         </div>
         <div className="Books-list">
           <button
@@ -66,22 +65,6 @@ const MainPage = () => {
               swiperRef.current = swiper;
             }}
             slidesPerView={2}
-          >
-            {displayedBooks.map((book) => (
-              <SwiperSlide key={book.id}>
-                <Link to={`/books/${book.name}`}>
-                  <img className="img" src={book.image} alt="book" />
-                  <h2>{book.name}</h2>
-                  <p>{book.category}</p>
-                  <p>{book.info}</p>
-                  <div className="icons">
-                    <FaFacebookF />
-                    <BsTwitter />
-                    <FaVimeoV />
-            onSwiper={(swiper) => {
-              swiperRef.current = swiper;
-            }}
-            slidesPerView={3}
           >
             {displayedBooks.map((book) => (
               <SwiperSlide key={book.id}>
