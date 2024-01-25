d40f37eebb2f682eed21a6cd0b5b641bf700508a
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-25 23:22:20 +0430

    create the deletebook file

---
+++ src/components/DeleteBook.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { removeBook } from '../redux/bookSlice';
+
+const DeleteBook = () =>
+// const dispatch = useDispatch();
+// const books = useSelector((state) => state.books);
+
+// const handleDelete = (bookId) => {
+//   dispatch(removeBook(bookId));
+// };
+  (
+    <>
+      <div className="delete-container">
+        <h2 className="title">Delete Books</h2>
+      </div>
+    </>
+  );
+export default DeleteBook;
