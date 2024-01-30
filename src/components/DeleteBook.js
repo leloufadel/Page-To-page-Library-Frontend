e4d739369b00dcb2fb2eded28b0c37f8a041b7ed
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-30 20:22:21 +0430

    add the delete massage

--- src/components/DeleteBook.js
+++ src/components/DeleteBook.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import { removeBook } from '../redux/bookSlice';
 
 const DeleteBook = () => {
@@ -8,6 +9,7 @@ const DeleteBook = () => {
 
   const handleDelete = (bookId) => {
     dispatch(removeBook(bookId));
+    toast.success('Book Deleted Successfully');
   };
 
   return (
